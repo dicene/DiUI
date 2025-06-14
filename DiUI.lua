023eeedf24e67615c57235e6388502fe1e858dc7
Dicene <ieadkins@gmail.com>
2025-06-14 16:28:11 -0400

    Added methods to DiWindow to generate widgets. Updated Button to be actually functional

--- DiUI.lua
+++ DiUI.lua
@@ -115,6 +115,46 @@ function DiWindow:Show()
     self.InternalWidget:AddToViewport(99999)
 end
 
+---@param text string
+---@return UTextBlock
+function DiWindow:CreateTextBlock(text)
+    return DiUI.CreateTextBlock(self.InternalWidget, text)
+end
+
+---@return UProgressBar
+function DiWindow:CreateProgressBar()
+    return DiUI.CreateProgressBar(self.InternalWidget)
+end
+
+---@param image UTexture2D
+---@param size FVector2D
+---@return UImage
+function DiWindow:CreateImage(image, size)
+    return DiUI.CreateImage(self.InternalWidget, image, size)
+end
+
+---@return UHorizontalBox
+function DiWindow:CreateHorizontalBox()
+    return DiUI.CreateHorizontalBox(self.InternalWidget)
+end
+
+---@return UVerticalBox
+function DiWindow:CreateVerticalBox()
+    return DiUI.CreateVerticalBox(self.InternalWidget)
+end
+
+---@return USizeBox
+function DiWindow:CreateSizeBox()
+    return DiUI.CreateSizeBox(self.InternalWidget)
+end
+
+---@param text string
+---@param clickFunction function
+---@return UWBP_ModernPrefab_Button_C
+function DiWindow:CreateButton(text, clickFunction)
+    return DiUI.CreateButton(self.InternalWidget, text, clickFunction)
+end
+
 ---Destroys all widgets matching a particular name. Be careful with this, and only use widget names that are VERY UNIQUE.
 ---@param name string
 function DiUI.DestroyWidgetsByName(name)
@@ -136,15 +176,36 @@ function DiUI.DestroyAllWidgets()
 end
 
 ---@param parent UWidget | DiWindow
+---@return UUserWidget
+function DiUI.CreateUserWidget(parent)
+    local parent = parent or CreateInvalidObject()
+    local parent = parent:IsValid() and parent.InternalWidget:IsValid() and parent.InternalWidget or parent ---@cast parent UWidget
+
+    if not parent:IsValid() then
+        return CreateInvalidObject()
+    end
+
+    local userWidget = StaticConstructObject(StaticFindObject("/Script/UMG.UserWidget"), parent) ---@cast userWidget UUserWidget
+
+    return userWidget
+end
+
+---@param parent UWidget | DiWindow
 ---@param name string
 ---@return UProgressBar
 function DiUI.CreateProgressBar(parent)
-    local parent = parent ~= nil and parent.InternalWidget ~= nil and parent.InternalWidget or parent ---@cast parent UWidget
+    local parent = parent or CreateInvalidObject()
+    local parent = parent:IsValid() and parent.InternalWidget:IsValid() and parent.InternalWidget or parent ---@cast parent UWidget
+
+    if not parent:IsValid() then
+        return CreateInvalidObject()
+    end
+
     local progressBar = StaticConstructObject(StaticFindObject("/Script/UMG.ProgressBar"), parent) ---@cast progressBar UProgressBar
     progressBar.ColorAndOpacity = {SpecifiedColor={A=1, R=0, G=0, B=0}, ColorUseRule=0}
     progressBar:SetPercent(0.80)
     progressBar:SetFillColorAndOpacity({A=1, R=0.1, G=1, B=0.0})
-    
+
     return progressBar
 end
 
@@ -152,8 +213,16 @@ end
 ---@param name string
 ---@param text string
 ---@return UTextBlock
-function DiUI.CreateTextBlock(parent, name, text)
-    local parent = parent ~= nil and parent.InternalWidget ~= nil and parent.InternalWidget or parent ---@cast parent UWidget
+function DiUI.CreateTextBlock(parent, text)
+    local parent = parent or CreateInvalidObject()
+    printf("Parent: %s", parent)
+    parent = parent:IsValid() and parent.InternalWidget:IsValid() and parent.InternalWidget or parent ---@cast parent UWidget
+
+    if not parent:IsValid() then
+        return CreateInvalidObject()
+    end
+
+    local text = text or "[Text]"
     local textBlock = StaticConstructObject(StaticFindObject("/Script/UMG.TextBlock"), parent) ---@cast textBlock UTextBlock
 
     textBlock:SetText(FText(text))
@@ -175,7 +244,13 @@ end
 ---@param size? FVector2D
 ---@return UImage
 function DiUI.CreateImage(parent, texture2d, size)
-    local parent = parent ~= nil and parent.InternalWidget ~= nil and parent.InternalWidget or parent ---@cast parent UWidget
+    local parent = parent or CreateInvalidObject()
+    local parent = parent:IsValid() and parent.InternalWidget:IsValid() and parent.InternalWidget or parent ---@cast parent UWidget
+
+    if not parent:IsValid() then
+        return CreateInvalidObject()
+    end
+
     local image = StaticConstructObject(StaticFindObject("/Script/UMG.Image"), parent) ---@cast image UImage
 
     if size == nil then
@@ -191,7 +266,13 @@ end
 ---@param parent UWidget | DiWindow
 ---@return UHorizontalBox
 function DiUI.CreateHorizontalBox(parent)
-    local parent = parent ~= nil and parent.InternalWidget ~= nil and parent.InternalWidget or parent ---@cast parent UWidget
+    local parent = parent or CreateInvalidObject()
+    local parent = parent:IsValid() and parent.InternalWidget:IsValid() and parent.InternalWidget or parent ---@cast parent UWidget
+
+    if not parent:IsValid() then
+        return CreateInvalidObject()
+    end
+
     local hBox = StaticConstructObject(StaticFindObject("/Script/UMG.HorizontalBox"), parent) ---@cast hBox UHorizontalBox
 
     return hBox
@@ -200,34 +281,116 @@ end
 ---@param parent UWidget | DiWindow
 ---@return UVerticalBox
 function DiUI.CreateVerticalBox(parent)
-    local parent = parent ~= nil and parent.InternalWidget ~= nil and parent.InternalWidget or parent ---@cast parent UWidget
+    local parent = parent or CreateInvalidObject()
+    local parent = parent:IsValid() and parent.InternalWidget:IsValid() and parent.InternalWidget or parent ---@cast parent UWidget
+
+    if not parent:IsValid() then
+        return CreateInvalidObject()
+    end
+
     local hBox = StaticConstructObject(StaticFindObject("/Script/UMG.VerticalBox"), parent) ---@cast hBox UVerticalBox
 
     return hBox
 end
 
 ---@param parent UWidget | DiWindow
+---@return USizeBox
+function DiUI.CreateSizeBox(parent)
+    local parent = parent or CreateInvalidObject()
+    local parent = parent:IsValid() and parent.InternalWidget:IsValid() and parent.InternalWidget or parent ---@cast parent UWidget
+
+    if not parent:IsValid() then
+        return CreateInvalidObject()
+    end
+
+    local sizeBox = StaticConstructObject(StaticFindObject("/Script/UMG.SizeBox"), parent) ---@cast sizeBox USizeBox
+
+    return sizeBox
+end
+
+---@type TMap<string, function>
+local ButtonClickCallbacks = {}
+
+---@type number
+local ButtonClickHook
+
+local function RegisterButtonClickHook()
+    if not ButtonClickHook then
+        ButtonClickHook = RegisterHook("Function /Script/CommonUI.CommonButtonBase:HandleButtonPressed", function(button)
+            local button = button:get()
+            local buttonName = button:GetFullName()
+            if ButtonClickCallbacks[buttonName] ~= nil then
+                ButtonClickCallbacks[buttonName]()
+            end
+        end)
+    end
+end
+
+-- if not (StaticFindObject("/Game/UI/Modern/Prefabs/Buttons/WBP_ModernPrefab_Button.WBP_ModernPrefab_Button_C") or CreateInvalidObject()):IsValid() then
+--     ExecuteInGameThread(function()
+--         LoadAsset("/Game/UI/Modern/Prefabs/Buttons/WBP_ModernPrefab_Button.WBP_ModernPrefab_Button_C")
+
+--         RegisterHook("Function /Game/UI/Modern/Prefabs/Buttons/WBP_ModernPrefab_Button.WBP_ModernPrefab_Button_C:OnInitButtonWidgets", function(button)
+--             printf("OnInitButtonWidgets()")
+--         end)
+--     end)
+-- else
+--     RegisterHook("Function /Game/UI/Modern/Prefabs/Buttons/WBP_ModernPrefab_Button.WBP_ModernPrefab_Button_C:OnInitButtonWidgets", function(button)
+--         printf("OnInitButtonWidgets()")
+--     end)
+-- end
+
+---@param parent UWidget | DiWindow
+---@param text string
+---@param clickFunction function
 ---@return UWBP_ModernPrefab_Button_C
-function DiUI.CreateButton(parent, text)
-    local parent = parent ~= nil and parent.InternalWidget ~= nil and parent.InternalWidget or parent ---@cast parent UWidget
-    local buttonClass = StaticFindObject("/Game/UI/Modern/Prefabs/Buttons/WBP_ModernPrefab_Button.WBP_ModernPrefab_Button_C")
+function DiUI.CreateButton(parent, text, clickFunction)
+    local parent = parent or CreateInvalidObject()
+    local parent = parent:IsValid() and parent.InternalWidget:IsValid() and parent.InternalWidget or parent ---@cast parent UWidget
+
+    if not parent:IsValid() then
+        return CreateInvalidObject()
+    end
+
+    local buttonClass = StaticFindObject("/Game/UI/Modern/Prefabs/Buttons/WBP_ModernPrefab_Button.WBP_ModernPrefab_Button_C") or CreateInvalidObject()
+
+    if not buttonClass:IsValid() then
+        printf("Loading button asset!")
+        LoadAsset("/Game/UI/Modern/Prefabs/Buttons/WBP_ModernPrefab_Button.WBP_ModernPrefab_Button_C")
+        buttonClass = StaticFindObject("/Game/UI/Modern/Prefabs/Buttons/WBP_ModernPrefab_Button.WBP_ModernPrefab_Button_C") or CreateInvalidObject()
+    end
+
+    if not buttonClass:IsValid() then
+        printf("Failed to load ModernPrefab_Button.")
+        return CreateInvalidObject()
+    end
     -- printf("ButtonClass: %s", buttonClass:GetFullName())
-    -- local button = StaticConstructObject(StaticFindObject("/Game/UI/Modern/Prefabs/Buttons/WBP_ModernPrefab_Button.WBP_ModernPrefab_Button_C"), parent) ---@cast button UWBP_ModernPrefab_Button_C
-    local button = StaticConstructObject(buttonClass, parent) ---@cast button UWBP_ModernPrefab_Button_C
+    local button = StaticConstructObject(buttonClass, parent) or CreateInvalidObject() ---@cast button UWBP_ModernPrefab_Button_C
 
-    button:SetButtonText(FText(text))
-    button.ShouldFocusOnHover = false
-    button.ShouldApplyFocusEffectOnHover = true
+    if button:IsValid() then
+        button:SetButtonText(FText(text))
+        button.ShouldFocusOnHover = false
+        button.ShouldApplyFocusEffectOnHover = true
+        button:SetVisibility(1)
 
-    ExecuteWithDelay(100, function()
-        button.ButtonTextWidget:SetColorAndOpacity({SpecifiedColor={R=1,G=1,B=1,A=1.000000}, ColorUseRule=0})
-        button.ButtonTextWidget:SetDefaultFontSize(22)
-        button.State_Hover_Left_Fill:SetColorAndOpacity({A=1,R=0.1,G=0.2,B=0.2})
-        button.State_Hover_Middle_FIll:SetColorAndOpacity({A=1,R=0.1,G=0.2,B=0.2})
-        button.State_Hover_Right_Fill:SetColorAndOpacity({A=1,R=0.1,G=0.2,B=0.2})
-    end)
+        RegisterButtonClickHook()
+        ButtonClickCallbacks[button:GetFullName()] = clickFunction
 
-    button:SetDesiredSizeInViewport({X=50,Y=50})
+        ExecuteAsync(function()
+            ExecuteInGameThread(function ()
+                if button.ButtonTextWidget:IsValid() then
+                    button.ButtonTextWidget:SetColorAndOpacity({SpecifiedColor={R=1,G=1,B=1,A=1.000000}, ColorUseRule=0})
+                    button.ButtonTextWidget:SetDefaultFontSize(22)
+                end
+                if button.State_Hover_Left_Fill:IsValid() then button.State_Hover_Left_Fill:SetColorAndOpacity({A=1,R=0.1,G=0.2,B=0.2}) end
+                if button.State_Hover_Middle_FIll:IsValid() then button.State_Hover_Middle_FIll:SetColorAndOpacity({A=1,R=0.1,G=0.2,B=0.2}) end
+                if button.State_Hover_Right_Fill:IsValid() then button.State_Hover_Right_Fill:SetColorAndOpacity({A=1,R=0.1,G=0.2,B=0.2}) end
+                button:SetVisibility(0)
+            end)
+        end)
+    end
+
+    -- button:SetDesiredSizeInViewport({X=50,Y=50})
 
     return button
 end
